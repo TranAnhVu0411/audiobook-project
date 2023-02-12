bb8005dc3b90ca005caf385f99e8ffc3a3cd15c1
TranAnhVu0411 <72057891+TranAnhVu0411@users.noreply.github.com>
2023-02-12 08:58:57 +0700

    update user controller and user route

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose'),
-User = require('../models/user');
+User = require('../models/user'),
+Comment = require('../models/comment'),
+Rating = require('../models/rating'),
+cloudinary = require("../config/cloudinary");
+
+const roleMapping = {
+    0: 'all',
+    1: 'user',
+    2: 'admin'
+}
 
 module.exports = {
     total: (req, res) => {
@@ -12,24 +21,85 @@ module.exports = {
     show: (req, res) => {
         User.findById(req.params.id).then(
             user => {
-                res.status(200).json({user: user});
+                Comment.find({user: req.params.id}).populate('book').then(
+                    comments => {
+                        Rating.find({user: req.params.id}).populate('book').then(
+                            ratings => {
+                                res.status(200).json({user: user, comments: comments, ratings: ratings});
+                            }
+                        ).catch(error => {
+                            res.status(500).json({error: error})
+                        })
+                    }
+                ).catch(error => {
+                    res.status(500).json({error: error})
+                })
             }
         ).catch(error => {
             res.status(500).json({error: error})
         })
     },
     update: (req, res) => {
-        let updateContent = {}
-        Object.keys(req.body).forEach(field => {
-            updateContent[field]=req.body[field]
-        })
-
-        User.findByIdAndUpdate(req.params.id, {$set:updateContent}).then(
-            user => {
-                res.status(200).json({user: user});
+        let userParams = {
+            username: req.body.username,
+        }
+        cloudinary.uploader.upload(req.file.path, {
+            folder: "audiobook/avatar", 
+            public_id: req.params.id,
+            format: 'png',
+        }).then(imgUrl => {
+            userParams.avatar=imgUrl.url
+            User.findByIdAndUpdate(req.params.id, {$set:userParams}).then(
+                user => {
+                    res.status(200).json({user: user});
+                }
+            ).catch(
+                error=>{
+                    console(1)
+                    console.log(error)
+                    res.status(500).json(error);
+                }
+            );
+        }).catch(
+            error=>{
+                console(2)
+                console.log(error)
+                res.status(500).json(error);
             }
-        ).catch(error => {
-            res.status(500).json({error: error})
-        })
-    }
+        );
+    },
+    index: (req, res) => {
+        // Thiết lập số lượng sách trong 1 trang
+        let perPage = 10;
+        let page = req.query.page||1;
+        let searchQuery = {}
+        if (req.query.username !== undefined){
+             searchQuery['username'] = { "$regex": req.query.username, "$options": "i" }
+        }
+        if (req.query.email !== undefined){
+            searchQuery['email'] = { "$regex": req.query.email, "$options": "i" }
+        }
+        if (req.query.role !== undefined) {
+            if (req.query.role != 0){
+                searchQuery['role'] = roleMapping[req.query.role]
+            }
+        }
+        User.find(searchQuery, {}).skip((perPage*page)-perPage).limit(perPage).then(
+            users => {
+                User.countDocuments(searchQuery).then(
+                    count => {
+                        res.status(200).json({users: users, pageCount: Math.ceil(count / perPage), total: count});
+                    }
+                ).catch(
+                    error => {
+                        res.status(500).json(error);
+                    }
+                )
+            }
+        ).catch(
+            error => {
+                res.status(500).json(error);
+            }
+        )
+    },
 }
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express'),
 userController = require('../controllers/userControllers'),
+upload = require("../config/multer"),
 router = express.Router();
 
-router.get('/:id', userController.show)
+router.get('/find', userController.index);
 router.get("/total", userController.total);
-router.put("/update/:id", userController.update);
+router.put("/update/:id", upload.single('image'), userController.update);
+router.get('/:id', userController.show)
 
 module.exports = router;
