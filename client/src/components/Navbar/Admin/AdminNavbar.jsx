9524a710fb5c77e02b42be4ef3caeaf3dfba969c
TranAnhVu0411 <72057891+TranAnhVu0411@users.noreply.github.com>
2023-02-13 09:06:36 +0700

    add comment management link to navbar

--- client/src/components/Navbar/Admin/AdminNavbar.jsx
+++ client/src/components/Navbar/Admin/AdminNavbar.jsx
@@ -1,39 +1,53 @@
 import React, {useState, useEffect} from "react";
 import { Link, useLocation } from "react-router-dom";
-import {MdMenuBook, MdOutlineSupervisedUserCircle, MdOutlineSpaceDashboard} from "react-icons/md"
+import {MdMenuBook, MdOutlineSupervisedUserCircle, MdSupervisedUserCircle, MdOutlineSpaceDashboard} from "react-icons/md"
 import {GoTriangleDown, GoTriangleUp} from 'react-icons/go'
 import {GiBookshelf} from 'react-icons/gi'
-import {BiBookAdd} from 'react-icons/bi'
+import {BiBookAdd, BiCommentDetail} from 'react-icons/bi'
 import "./style.scss";
 const AdminNavbar = (props) => {
     // Phục vụ cho menu quản lý sách
     const [openBM, setOpenBM] = useState(false);
+    const [openUM, setOpenUM] = useState(false);
     const location = useLocation()
 
     useEffect(() => {
         setOpenBM(false);
+        setOpenUM(false)
     }, [location])
 
     // Trạng thái click (Khi màn hình nhỏ hơn 992)
-    const handleClickBM = () => {
+    const handleClick = (type) => {
         if (props.width>992){
             return null
         }else{
-            setOpenBM(!openBM)
+            if (type==='book'){
+                setOpenBM(!openBM)
+            }else{
+                setOpenUM(!openUM)
+            }
         }
     }
 
     // Trạng thái hover (Khi màn hình lón hơn 992)
-    const handleMouseOverBM = () => {
+    const handleMouseOver = (type) => {
         if (props.width>992){
-            setOpenBM(true)
+            if (type==='book'){
+                setOpenBM(true)
+            }else{
+                setOpenUM(true)
+            }
         }else{
             return null
         }
     }
-    const handleMouseOutBM = () => {
+    const handleMouseOut = (type) => {
         if (props.width>992){
-            setOpenBM(false)
+            if (type==='book'){
+                setOpenBM(false)
+            }else{
+                setOpenUM(false)
+            }
         }else{
             return null
         }
@@ -52,9 +66,22 @@ const AdminNavbar = (props) => {
         viewBMMode.display = "none"
     }
 
+    let viewUMMode = {}
+    if(openUM){
+        viewUMMode.display = "flex"
+        viewUMMode.gap = 30;
+        if (props.width > 992){
+            viewUMMode.flexDirection = "column"
+        }else{
+            viewUMMode.flexDirection = "row"
+        }
+    }else{
+        viewUMMode.display = "none"
+    }
+
     // Chỉnh icon khi ấn vào Quản lý sách 
-    const handleIconBM = () => {
-        if(openBM){
+    const handleIcon = (open) => {
+        if(open){
             return <GoTriangleUp className="icon triangle"/>
         }else{
             return <GoTriangleDown className="icon triangle"/>
@@ -67,29 +94,64 @@ const AdminNavbar = (props) => {
                 <MdOutlineSpaceDashboard className="icon"/>
                 <h6>Dashboard</h6>
             </Link>
-            <div className="book-management">
-                <button className="nav-link" onClick={handleClickBM} onMouseOver={handleMouseOverBM} onMouseOut={handleMouseOutBM}>
+            <div className="dropdown-nav">
+                <button 
+                    className="nav-link" 
+                    onClick={() => handleClick("book")} 
+                    onMouseOver={() => handleMouseOver("book")} 
+                    onMouseOut={() => handleMouseOut("book")}
+                >
                     <MdMenuBook className="icon"/>
                     <h6>Quản lý sách</h6>
                     {
-                        handleIconBM()
+                        handleIcon(openBM)
                     }
                 </button>
-                <div className="book-management-content" style={viewBMMode} onMouseOver={handleMouseOverBM} onMouseOut={handleMouseOutBM}>
-                    <Link className="book-management-link" to = '/booklist'>
+                <div 
+                    className="dropdown-nav-content" 
+                    style={viewBMMode} 
+                    onMouseOver={() => handleMouseOver("book")} 
+                    onMouseOut={() => handleMouseOut("book")}
+                >
+                    <Link className="dropdown-nav-link" to = '/booklist'>
                         <GiBookshelf className='icon' /> 
                         <span>Danh sách sách</span>
                     </Link>
-                    <Link className="book-management-link" to = '/book/new'>
+                    <Link className="dropdown-nav-link" to = '/book/new'>
                         <BiBookAdd className='icon' />
                         <span>Thêm sách mới</span>
                     </Link>
                 </div>
             </div>
-            <Link className="nav-link" to="/userlist">
-                <MdOutlineSupervisedUserCircle className="icon"/>
-                <h6>Quản lý tài khoản</h6>
-            </Link>
+            <div className="dropdown-nav">
+                <button 
+                    className="nav-link" 
+                    onClick={() => handleClick("user")} 
+                    onMouseOver={() => handleMouseOver("user")} 
+                    onMouseOut={() => handleMouseOut("user")}
+                >
+                    <MdOutlineSupervisedUserCircle className="icon"/>
+                    <h6>Quản lý tài khoản</h6>
+                    {
+                        handleIcon(openUM)
+                    }
+                </button>
+                <div 
+                    className="dropdown-nav-content" 
+                    style={viewUMMode} 
+                    onMouseOver={() => handleMouseOver("user")} 
+                    onMouseOut={() => handleMouseOut("user")}
+                >
+                    <Link className="dropdown-nav-link" to = '/userlist'>
+                        <MdSupervisedUserCircle className='icon' /> 
+                        <span>Quản lý tài khoản</span>
+                    </Link>
+                    <Link className="dropdown-nav-link" to = '/comment'>
+                        <BiCommentDetail className='icon' />
+                        <span>Quản lý đánh giá</span>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
